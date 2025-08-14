60da9f1379001addb9f2518e011b3903914cbeb8
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-14 14:43:48 +0530

    Third change

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@
 void Terminate() {
     exit(0);
 }
+
+void Create(const std::string &fileName) {
+    std::ofstream file(fileName);
+}
+
 bool fileExists(const std::string &fileName) {
     std::ifstream file(fileName);
     return file.is_open();
 }
+
 bool isFileEmpty(const std::string &fileName) {
     std::ifstream file(fileName);
     return file.peek() == std::ifstream::traits_type::eof();
 }
+
 int main(){
       std::string A = "Email.csv"; // Source file
     std::string B = "Destination_File.csv"; // Destination file
@@ -24,11 +31,15 @@ int main(){
     } else {
         std::cout << "[INFO] " << A << " found.\n";
     }
+
+    //2. check if A.csv is empty
     if (isFileEmpty(A)) {
         std::cerr << "[ERROR] " << A << " is empty. Terminating.\n";
         Terminate();
     } else {
         std::cout << "[INFO] " << A << " has data.\n";
     }
+    
+    
  
 }
