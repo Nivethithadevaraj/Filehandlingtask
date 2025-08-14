c9571d0689058b1ea01232939bee58ea3ecd217a
Nivethithadevaraj <nivethitha.devaraj@psiog.com>
2025-08-14 14:38:08 +0530

    second change

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ bool fileExists(const std::string &fileName) {
     std::ifstream file(fileName);
     return file.is_open();
 }
+bool isFileEmpty(const std::string &fileName) {
+    std::ifstream file(fileName);
+    return file.peek() == std::ifstream::traits_type::eof();
+}
 int main(){
       std::string A = "Email.csv"; // Source file
     std::string B = "Destination_File.csv"; // Destination file
@@ -20,4 +24,11 @@ int main(){
     } else {
         std::cout << "[INFO] " << A << " found.\n";
     }
+    if (isFileEmpty(A)) {
+        std::cerr << "[ERROR] " << A << " is empty. Terminating.\n";
+        Terminate();
+    } else {
+        std::cout << "[INFO] " << A << " has data.\n";
+    }
+ 
 }
